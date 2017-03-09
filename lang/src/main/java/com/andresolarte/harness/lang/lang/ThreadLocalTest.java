5426f332d1b3bd1885ea808c9cc3a29e0e1efd4d
Andres Olarte <aolarte@shoppertrak.com>
2017-03-09 12:29:00 -0600

    ThreadLocal test

---
+++ lang/src/main/java/com/andresolarte/harness/lang/lang/ThreadLocalTest.java
@@ -0,0 +1,32 @@
+package com.andresolarte.harness.lang.lang;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.stream.IntStream;
+
+public class ThreadLocalTest {
+
+    private static final ThreadLocal<SimpleDateFormat> dateFormat = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            System.out.println("Creating for thread: " + Thread.currentThread().getId());
+            return new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a");
+        }
+
+        @Override
+        public SimpleDateFormat get() {
+            System.out.println("Getting for thread: " + Thread.currentThread().getId());
+            return super.get();
+        }
+    };
+
+    public static void main(String... args) {
+        IntStream.range(0, 20).forEach(i -> new Thread(() -> {
+            SimpleDateFormat sdf = dateFormat.get();
+            System.out.println("Result: " + sdf.format(new Date()) +
+                    " thread: " + Thread.currentThread().getId() + " sdf: " + System.identityHashCode(sdf));
+
+        }).start());
+    }
+
+}
